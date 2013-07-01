0644d70e2a8814908eb264e10a99f17f320fcdd0
MakerSquare Student <student@macintosh.att.net>
2013-07-01 09:38:07 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MattBlog::Application.config.secret_key_base = '371a45ba50ac72b94b92eec376cbe54144912e47e0b3e6d005af5363a9443b29abbc5ec1fa60367bb0575dcdf9402649bb7709e313aeb943a9439cda871a51c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MattBlog::Application.config.session_store :cookie_store, key: '_matt_blog_session'
